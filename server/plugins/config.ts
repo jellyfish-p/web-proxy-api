1e613b0fe29760d503b640a1943eb215cdbb94a0
jellyfish-p <jellyfish_p@vip.qq.com>
2026-02-01 09:17:39 +0800

    删库跑路

--- server/plugins/config.ts
+++
@@ -1,7 +0,0 @@
-import { loadConfig } from '../utils/config'
-
-export default defineNitroPlugin(() => {
-  // 在服务器启动时加载配置，只执行一次
-  loadConfig()
-  console.log('⚙️ Server initialized with config')
-})
